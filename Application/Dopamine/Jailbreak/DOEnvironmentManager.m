43c7480e352c9c0eca2205c57ae003a38284bb2b
Ellie of the Winters Collective <121464006+ElliesSurviving@users.noreply.github.com>
2024-06-14 17:48:19 +0100

    ruby wanted this

--- Application/Dopamine/Jailbreak/DOEnvironmentManager.m
+++ Application/Dopamine/Jailbreak/DOEnvironmentManager.m
@@ -195,10 +195,10 @@ int reboot3(uint64_t flags, ...);
 - (NSString *)versionSupportString
 {
     if ([self isArm64e]) {
-        return @"iOS 15.0 - 16.5.1 (arm64e), v2.1.7.7 (custom)";
+        return @"iOS 15.0 - 16.5.1 (arm64e), v2.1.7.8 (custom)";
     }
     else {
-        return @"iOS 15.0 - 16.6.1 (arm64), v2.1.7.7 (custom)";
+        return @"iOS 15.0 - 16.6.1 (arm64), v2.1.7.8 (custom)";
     }
 }
 
