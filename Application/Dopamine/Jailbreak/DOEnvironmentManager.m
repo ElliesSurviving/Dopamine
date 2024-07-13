57a6f9edefe99e4f1583f4bd5012d802becb3173
Ellie of the Winters Collective <121464006+ElliesSurviving@users.noreply.github.com>
2024-07-13 11:14:58 +0100

    i love compilation errors

--- Application/Dopamine/Jailbreak/DOEnvironmentManager.m
+++ Application/Dopamine/Jailbreak/DOEnvironmentManager.m
@@ -146,7 +146,7 @@ int reboot3(uint64_t flags, ...);
         
         if (randomizedJailbreakPath) {
             NSString *jailbreakRootPath = [randomizedJailbreakPath stringByAppendingPathComponent:@"procursus"];
-            NSString *JailbreakRootPathWinters = [JailbreakRootPath stringByAppendingPathComponent:@".installed_winters"]
+            NSString *JailbreakRootPathWinters = [randomizedJailbreakPath stringByAppendingPathComponent:@"procursus/.installed_winters"];
             if ([[NSFileManager defaultManager] fileExistsAtPath:jailbreakRootPath] && [[NSFileManager defaultManager] fileExistsAtPath:jailbreakRootPathWinters]) {
                 // This attribute serves as the primary source of what the root path is
                 // Anything else in the jailbreak will get it from here
