a30ddfa26d70709656212845b7d845e29ba056fd
Ellie of the Winters Collective <121464006+ElliesSurviving@users.noreply.github.com>
2024-08-22 09:19:19 +0100

    frcoal

--- BaseBin/systemhook/src/common.h
+++ BaseBin/systemhook/src/common.h
@@ -14,12 +14,11 @@ typedef enum
 int __posix_spawn(pid_t *restrict pid, const char *restrict path, struct _posix_spawn_args_desc *desc, char *const argv[restrict], char *const envp[restrict]);
 int __execve(const char *path, char *const argv[], char *const envp[]);
 
-
-
 bool string_has_prefix(const char *str, const char* prefix);
 bool string_has_suffix(const char* str, const char* suffix);
 
 int __posix_spawn_orig(pid_t *restrict pid, const char *restrict path, struct _posix_spawn_args_desc *desc, char *const argv[restrict], char * const envp[restrict]);
+int __execve_orig(const char *path, char *const argv[], char *const envp[]);
 
 int resolvePath(const char *file, const char *searchPath, int (^attemptHandler)(char *path));
 int posix_spawn_hook_shared(pid_t *restrict pid, const char *restrict path, struct _posix_spawn_args_desc *desc, char *const argv[restrict], char *const envp[restrict], void *orig, int (*trust_binary)(const char *path, xpc_object_t preferredArchsArray), int (*set_process_debugged)(uint64_t pid, bool fullyDebugged), double jetsamMultiplier);
